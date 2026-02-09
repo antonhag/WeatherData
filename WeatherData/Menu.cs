54dd98863419e045501ea31db37bcf177da3e4cf
antonhag <anton.hagstrom@live.com>
2026-02-09 11:53:08 +0100

    Added files

---
+++ WeatherData/Menu.cs
@@ -0,0 +1,78 @@
+namespace WeatherData;
+
+public class Menu
+{
+    private enum MenuOption
+    {
+        Utomhus = 1,
+        Inomhus = 2
+    }
+    
+    private enum OutsideInfo
+    {
+        Visa_medeltemperatur_och_luftfuktighet_per_dag_för_valt_datum = 1,
+        Sortera_varmast_till_kallaste_dagen_enligt_medeltemperatur_per_dag,
+        Sortera_torrast_till_fuktigast_dagen_enligt_medelfuktighet_per_dag,
+        Sortera_minst_till_störst_risk_av_mögel,
+        Visa_datum_för_meteorologisk_höst,
+        Visa_datum_för_meteorologisk_vinter,
+        Återgå_till_menyn = 9
+    }
+
+    private enum InsideInfo
+    {
+        Visa_medeltemperatur_för_valt_datum = 1,
+        Sortera_varmast_till_kallaste_dagen_enligt_medeltemperatur_per_dag,
+        Sortera_torrast_till_fuktigast_dagen_enligt_medelfuktighet_per_dag,
+        Sortera_minst_till_störst_risk_av_mögel,
+        Återgå_till_menyn = 9
+    }
+
+    public static void ShowMenu()
+    {
+        var options = new List<string>();
+        
+        foreach (int i in Enum.GetValues(typeof(MenuOption)))
+        {
+            options.Add($"{i}. {Enum.GetName(typeof(MenuOption), i).Replace("_", " ")}");
+        }
+
+
+        Console.WriteLine("Välj vad du vill få info om\n");
+        foreach (var option in options)
+        {
+            Console.WriteLine(option);
+        }
+    }
+
+    public static void ShowOutsideMenu()
+    {
+        var options = new List<string>();
+        
+        foreach (int i in Enum.GetValues(typeof(OutsideInfo)))
+        {
+            options.Add($"{i}. {Enum.GetName(typeof(OutsideInfo), i).Replace("_", " ")}");
+        }
+        
+        foreach (var option in options)
+        {
+            Console.WriteLine(option);
+        }
+    }
+
+    public static void ShowInsideMenu()
+    {
+        var options = new List<string>();
+        
+        foreach (int i in Enum.GetValues(typeof(InsideInfo)))
+        {
+            options.Add($"{i}. {Enum.GetName(typeof(InsideInfo), i).Replace("_", " ")}");
+        }
+
+        foreach (var option in options)
+        {
+            Console.WriteLine(option);
+        }
+    }
+    
+}
---
+++ WeatherData/Program.cs
@@ -0,0 +1,105 @@
+﻿namespace WeatherData;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.Clear();
+            Menu.ShowMenu();
+        
+            var key =  Console.ReadKey(true).KeyChar;
+
+            switch (key)
+            {
+                case '1':
+                    Console.Clear();
+                    Console.WriteLine("Tryck på siffran för det du vill ha info om\n");
+                    Menu.ShowOutsideMenu();
+                   
+                    
+                    var key2 = Console.ReadKey(true).KeyChar;
+
+                    switch (key2)
+                    {
+                        case '1':
+                            Console.Clear();
+                            Console.WriteLine("test metod");
+                            Console.ReadKey();
+                            break;
+                        case '2':
+                            Console.Clear();
+                            // metod
+                            break;
+                        case '3':
+                            Console.Clear();
+                            // metod
+                            break;
+                        case '4':
+                            Console.Clear();
+                            // metod
+                            break;
+                        case '5':
+                            Console.Clear();
+                            // metod
+                            break;
+                        case '6':
+                            Console.Clear();
+                            // metod
+                            break;
+                        case '9':
+                            break;
+                        default:
+                            Console.WriteLine("Felaktigt val, tryck på valfri knapp för att gå vidare...");
+                            Console.ReadKey();
+                            break;
+                    }
+                    break;
+                case '2':
+                    Console.Clear();
+                    Console.WriteLine("Tryck på siffran för det du vill ha info om\n");
+                    Menu.ShowInsideMenu();
+                    
+                    var key3 = Console.ReadKey(true).KeyChar;
+
+                    switch (key3)
+                    {
+                        case '1':
+                            Console.Clear();
+                            Console.WriteLine("test metod");
+                            Console.ReadKey();
+                            break;
+                        case '2':
+                            Console.Clear();
+                            // metod
+                            break;
+                        case '3':
+                            Console.Clear();
+                            // metod
+                            break;
+                        case '4':
+                            Console.Clear();
+                            // metod
+                            break;
+                        case '5':
+                            Console.Clear();
+                            // metod
+                            break;
+                        case '9':
+                            // metod
+                            break;
+                        default:
+                            Console.WriteLine("Felaktigt val, tryck på valfri knapp för att gå vidare...");
+                            Console.ReadKey();
+                            break;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Felaktigt val, tryck på valfri knapp för att gå vidare...");
+                    Console.ReadKey();
+                    break;
+            }
+        }
+    }
+}
