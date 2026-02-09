6fc4789ed5786ec18d43fdeead6a2b4c4699c635
Hamed Jafari <hamed.jafari2003@gmail.com>
2026-02-09 14:31:51 +0100

    Made a WeatherServices map with outside and inside classes for the methods

---
+++ WeatherData/Helpers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherData
+{
+    internal class Helpers
+    {
+        //public static double GetAvrageTemp 
+        //{
+            
+        //}
+        2016-05-31 13:58:30,Inne,24.8,42
+
+    }
+}
